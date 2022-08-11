187ec9849b6296d197aa80e01bef0ee747a86ff9
Olga Fedorova <bamnu13@gmail.com>
2022-08-11 17:04:41 +0300

    Update Main.java - added MagicBox feature

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MagicBox<Integer> integerBox = new MagicBox<>("'Magic Box for Numbers'", new Integer[3]);
+        MagicBox<String> stringBox = new MagicBox<>("'Magic Box for Words'", new String[3]);
+
+        integerBox.add(1);
+        integerBox.add(2);
+        integerBox.add(3);
+        integerBox.add(4);
+
+        stringBox.add("letter");
+        stringBox.add("word");
+        stringBox.add("sentence");
+        stringBox.add("text");
+
+        System.out.println(integerBox + "\n" + stringBox);
     }
 }
